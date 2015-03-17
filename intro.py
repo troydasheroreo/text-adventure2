03bc8129c5b1c2ee6595795af387f03e7869a564
Troy Miller <troydasheroreo@gmail.com>
2015-03-17 17:14:02 -0400

    save

---
+++ intro.py
@@ -0,0 +1,6 @@
+'''Welcome(Introduction)
+
+Are you ready for this? (Plays Whoomp there it is by Tag Team).
+No, but in all seriouness are you ready to play my game, I hope you enjoy it.
+Take it seriously please.
+'''
